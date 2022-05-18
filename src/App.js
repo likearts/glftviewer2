fcf14ae7ee7da852542499b48ea9a0cb01068c79
likearts <likearts@naver.com>
2022-05-18 22:15:57 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF(
+    "https://storage.googleapis.com/webar-dev-bd3d4.appspot.com/Owner/asus/SA002-TW-21110019/Zen_UX8402_Laptop_Def_Def/Zen_UX8402_Laptop_Def_Def.gltf"
+  );
+  return <primitive object={scene} />;
+}
+
+export default function App() {
+  return (
+    <Canvas pixelRatio={[1, 2]} camera={{ position: [-10, 15, 15], fov: 50 }}>
+      <ambientLight intensity={1} />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+}
